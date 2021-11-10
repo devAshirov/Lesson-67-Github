4f1555ab003d94604cb72c693bea9a1c8ecd1a1e
devAshirov <siriusgold5444@gmail.com>
2021-11-10 16:26:08 +0500

    my first change code

--- Lesson 67/ViewController.swift
+++ Lesson 67/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    func firtFunc() {
+        print("Hello")
+    }
+    
+    
+    
 }
 
