cf80213b89bdc511ea7b71ab18d1da34171bbc72
imuhlisa <muhlisaiosdev@gmail.com>
2021-11-10 18:03:03 +0500

    newVC

---
+++ Lesson 67/newVC.swift
@@ -0,0 +1,29 @@
+//
+//  newVC.swift
+//  Lesson 67
+//
+//  Created by Muhlisa's MacBook on 10/11/21.
+//
+
+import UIKit
+
+class newVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
