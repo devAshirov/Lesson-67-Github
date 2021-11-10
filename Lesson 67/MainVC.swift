190bd2e0115e746a065bdc25dd993ef94875ac4d
imuhlisa <muhlisaiosdev@gmail.com>
2021-11-10 17:57:20 +0500

    my first VC

---
+++ Lesson 67/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  Lesson 67
+//
+//  Created by Muhlisa's MacBook on 10/11/21.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+   
+
+}
